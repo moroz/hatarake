8e4630c10580a331d16e9d0d6925b668b7ada993
Karol Moroz <dmuhafc@gmail.com>
2017-04-12 18:16:35 +0800

    Create a static signup page for employer/candidate

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :employers
   devise_for :users
   root to: 'pages#home'
+  get '/sign_up', to: 'pages#signup'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
