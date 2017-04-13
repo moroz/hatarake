6d954cc9764a5489b7f1bc90172b0fb2b47ef9c6
Karol Moroz <dmuhafc@gmail.com>
2017-04-13 15:55:03 +0800

    Add name and birth date to User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170413042836) do
+ActiveRecord::Schema.define(version: 20170413073549) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,9 @@ ActiveRecord::Schema.define(version: 20170413042836) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "type"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.date     "birth_date"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
