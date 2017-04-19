75b1e10b9f003521a6e50f085801784118a9dc34
Karol Moroz <dmuhafc@gmail.com>
2017-04-19 18:47:27 +0800

    Make seeds recursively load db/seeds/*

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+files = Dir[File.expand_path(Rails.root + "db/seeds/*")]
+files.each do |f|
+  load f
+end
