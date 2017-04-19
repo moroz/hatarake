cffaa157c64b8419f053737d58323727dca7aa44
Karol Moroz <dmuhafc@gmail.com>
2017-04-19 18:34:29 +0800

    Add model Admin and helper method admin?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  helper_method :current_user, :admin
 
   #def after_sign_in_path_for(resource)
     #profile_path
   #end
-
-  private
-
   def current_user
     super || current_candidate || current_company
   end
+
+  def admin?
+    current_user && current_user.type == "Admin"
+  end
+
+  private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:first_name, :last_name])
