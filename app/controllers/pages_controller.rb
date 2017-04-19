3250b3c0e85e86338f48c5623643ac1a684b2b71
Karol Moroz <dmuhafc@gmail.com>
2017-04-19 17:57:13 +0800

    Add CRU actions to PagesController

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,32 @@
 class PagesController < ApplicationController
+  expose :page, fetch: -> { fetch_page }
   def show
-    if params[:permalink].present?
-      @page = Page.find_by(permalink: params[:permalink])
-      raise ActiveRecord::RecordNotFound if @page.nil?
+  end
+
+  def edit
+    @title = "Editing page: #{page.title}"
+    render 'new'
+  end
+
+  def update
+    if page.update(page_params)
+      flash[:success] = "The page was updated."
+      redirect_to page
     end
+  end
+
+  private
+
+  def fetch_page
+    permalink = params[:permalink] || params[:id]
+    if permalink.present?
+      @page = Page.find_by(permalink: permalink)
+      raise ActiveRecord::RecordNotFound if @page.nil?
+      @page
+    end
+  end
+
+  def page_params
+    params.require(:page).permit(:title, :body, :permalink)
   end
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,3 +1,14 @@
 class Page < ApplicationRecord
-  validates :permalink, uniqueness: true
+  validates :permalink, uniqueness: true, presence: true
+  before_save :strip_lines
+
+  def strip_lines
+    self.body = body.lines.map { |l|
+      l.strip
+    }.join("\n")
+  end
+
+  def to_param
+    permalink
+  end
 end
