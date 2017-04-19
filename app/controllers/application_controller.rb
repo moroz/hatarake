311a89836e1afe12ac51ee80edc517685fb91354
Karol Moroz <dmuhafc@gmail.com>
2017-04-19 18:40:05 +0800

    Add helpers to check user type

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-  helper_method :current_user, :admin
+  helper_method :current_user, :admin?, :company?, :candidate?
 
   #def after_sign_in_path_for(resource)
     #profile_path
@@ -11,10 +11,22 @@ class ApplicationController < ActionController::Base
   end
 
   def admin?
-    current_user && current_user.type == "Admin"
+    current_user_is_a? "Admin"
+  end
+
+  def company?
+    current_user_is_a? "Company"
+  end
+
+  def candidate?
+    current_user_is_a? "Candidate"
   end
 
   private
+
+  def current_user_is_a?(type)
+    current_user && current_user.type == type
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:first_name, :last_name])
