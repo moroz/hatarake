e23dcfdcb751dce868c74d5bd616e45007a97168
Karol Moroz <dmuhafc@gmail.com>
2017-04-19 17:06:59 +0800

    Create model Page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170418061954) do
+ActiveRecord::Schema.define(version: 20170419090606) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,14 @@ ActiveRecord::Schema.define(version: 20170418061954) do
   create_table "organizations", force: :cascade do |t|
     t.string   "name"
     t.string   "name_pl"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "pages", force: :cascade do |t|
+    t.string   "title"
+    t.string   "permalink"
+    t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
---
+++ spec/factories/pages.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :page do
+    title "MyString"
+    permalink "MyString"
+    body "MyText"
+  end
+end
