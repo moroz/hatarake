2af07bb75bbd35bc665090ba77ba5c39cdfcf336
Karol Moroz <dmuhafc@gmail.com>
2017-04-19 17:23:42 +0800

    Stub specs for Page

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,3 @@
 class Page < ApplicationRecord
+  validates :permalink, uniqueness: true
 end
--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Page, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations" do
+    describe "permalink" do
+      context "with unique permalink" do
+        it "is valid"
+      end
+
+      context "with an existing permalink" do
+        it "is invalid"
+      end
+
+      context "without permalink" do
+        it "is invalid"
+      end
+    end
+  end
 end
