d573d1f175321202b4a3c1d9988378855c4bb45a
Karol Moroz <dmuhafc@gmail.com>
2017-04-19 18:30:59 +0800

    Add actions for managing offers

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -7,4 +7,46 @@ class OffersController < ApplicationController
 
   def index
   end
+
+  def edit
+    @title = "Editing offer: #{offer.title}"
+    render 'new'
+  end
+  
+  def create
+    if @offer = current_company.offers.create(offer_params)
+      redirect_to @offer
+    else
+      render :new, offer: offer
+    end
+  end
+
+  def update
+    if offer.update(offer_params)
+      flash[:success] = "The offer was updated."
+      redirect_to offer
+    else
+      @title = "Editing offer: #{offer.title}"
+      render 'new'
+    end
+  end
+
+  def destroy
+    title = offer.title
+    if offer.destroy
+      flash[:success] = "Offer \"#{title}\" has been deleted."
+      redirect_to offers_path
+    end
+  end
+
+  private
+
+  def current_company
+    super || Company.first # only testing
+  end
+
+  def offer_params
+    params.require(:offer).permit(:title, :currency, :salary_min,
+                                  :salary_max, :contact_email, :contact_phone, :location, :description)
+  end
 end
