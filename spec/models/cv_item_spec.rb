a98f7cd49d0fcb7f5ebe13a5a4a540699963903d
Karol Moroz <dmuhafc@gmail.com>
2017-04-21 20:15:21 +0800

    Refactor CvItem specs to work for education and work items

--- spec/models/cv_item_spec.rb
+++ spec/models/cv_item_spec.rb
@@ -1,18 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe CvItem, type: :model do
-  let(:cv_item) { FactoryGirl.build(:cv_item) }
-  let(:work_item) { FactoryGirl.build(:cv_item, :work) }
+RSpec.shared_examples "cv item" do
+  def factory_key
+    described_class.model_name.singular
+  end
+
+  let(:item) { FactoryGirl.build(factory_key) }
+
   describe "validations" do
     context "with valid attributes" do
       it "is valid" do
-        expect(cv_item).to be_valid
+        expect(item).to be_valid
       end
     end
 
     context "with no dates" do
       it "is invalid" do
-        no_date_item = FactoryGirl.build(:cv_item, :no_dates)
+        no_date_item = FactoryGirl.build(factory_key, :no_dates)
         expect(no_date_item).not_to be_valid
       end
     end
@@ -22,7 +26,7 @@ RSpec.describe CvItem, type: :model do
     describe "make_dates" do
       context "when given start month and year" do
         it "converts them to Date" do
-          start_date_item = FactoryGirl.build(:cv_item, :no_dates)
+          start_date_item = FactoryGirl.build(factory_key, :no_dates)
           start_date_item.start_month = "9"
           start_date_item.start_year = "2000"
           start_date_item.validate
@@ -32,7 +36,7 @@ RSpec.describe CvItem, type: :model do
 
       context "when given end month and year" do
         it "converts them to Date" do
-          end_date_item = FactoryGirl.build(:cv_item, :no_dates)
+          end_date_item = FactoryGirl.build(factory_key, :no_dates)
           end_date_item.end_month = "9"
           end_date_item.end_year = "2000"
           end_date_item.validate
@@ -44,37 +48,20 @@ RSpec.describe CvItem, type: :model do
     describe "swap_dates" do
       context "when end date is before start date" do
         it "swaps them" do
-          cv_item.start_date = 2.years.ago
-          cv_item.end_date = 3.years.ago
-          cv_item.validate
-          expect(cv_item.end_date).to be < cv_item.start_date
+          item.start_date = 2.years.ago
+          item.end_date = 3.years.ago
+          item.validate
+          expect(item.end_date).to be < item.start_date
         end
       end
     end
   end
+end
 
-  describe "scopes" do
-    let!(:edu_item) { FactoryGirl.create(:cv_item, category: "ba") }
-    let!(:work_item) { FactoryGirl.create(:cv_item, :work) }
+RSpec.describe EducationItem do
+  include_examples "cv item"
+end
 
-    describe "education_items" do
-      it "does include educational items" do
-        expect(CvItem.education_items).to include edu_item
-      end
-
-      it "does not include work items" do
-        expect(CvItem.education_items).not_to include work_item
-      end
-    end
-
-    describe "work_items" do
-      it "does include work items" do
-        expect(CvItem.work_items).to include work_item
-      end
-
-      it "does not include educational items" do
-        expect(CvItem.work_items).not_to include edu_item
-      end
-    end
-  end
+RSpec.describe WorkItem do
+  include_examples "cv item"
 end
