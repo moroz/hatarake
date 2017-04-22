511ad2e459ae069b0a29ff2cc7f0b3e44b0d4a97
Karol Moroz <dmuhafc@gmail.com>
2017-04-22 16:39:35 +0800

    Add view for companies#show

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,6 +1,26 @@
 class CompaniesController < ApplicationController
+  before_action :find_user
+  helper_method :company
+
   expose :companies { Company.all }
   def index
 
+  end
+
+  def show
+  end
+
+  private
+
+  def company
+    @company ||= find_user
+  end
+
+  def find_user
+    if params[:id].present?
+      @company = User.find(params[:id])
+    elsif signed_in?
+      @company = current_user
+    end
   end
 end
