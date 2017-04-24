575c00515d13587ca367bbfc500770adfdefe495
Karol Moroz <dmuhafc@gmail.com>
2017-04-24 20:12:14 +0800

    Make Profession autocompletable

--- app/models/profession.rb
+++ app/models/profession.rb
@@ -1,5 +1,6 @@
 class Profession < ApplicationRecord
   extend FindOrCreate
+  extend ActsLikeAutocompletable
 
   has_many :candidates
 end
--- spec/models/profession_spec.rb
+++ spec/models/profession_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
+require './spec/support/autocompletable'
 
 RSpec.describe Profession, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  include_examples "acts like autocompletable"
 end
