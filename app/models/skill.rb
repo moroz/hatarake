1e97cdb8cfb71ad08e364a12fa5836935d9afe90
Karol Moroz <dmuhafc@gmail.com>
2017-04-28 13:56:08 +0800

    Change colors, fix validation in Skill

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -2,7 +2,7 @@ class Skill < ApplicationRecord
   has_many :skill_items
   has_and_belongs_to_many :offers
   validates :name, uniqueness: true, presence: true
-  validates :name_pl, uniqueness: true
+  validates :name_pl, uniqueness: true, allow_blank: true
   extend FindOrCreate
   extend ActsLikeAutocompletable
 end
