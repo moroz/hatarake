4d6821dd6dc0460f2b8812b47e423b7452d6382c
Karol Moroz <dmuhafc@gmail.com>
2017-04-30 21:39:56 +0800

    Translate offers#show to pl

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,7 @@ module InJobs
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    I18n.available_locales = [:en, :pl]
+    I18n.default_locale = :pl
   end
 end
