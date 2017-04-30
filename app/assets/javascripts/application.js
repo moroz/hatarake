3391eee5cdd3926098938110446b72da2c6e0890
Karol Moroz <dmuhafc@gmail.com>
2017-04-30 13:02:16 +0800

    Add preview of textilized content in offers#new

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require jquery-ui/widgets/autocomplete
 //= require jquery-readyselector
+//= require textile
 //= require autocomplete-rails
 //= require foundation
 //= require_tree .
--- app/assets/javascripts/offers.js
+++ app/assets/javascripts/offers.js
@@ -7,4 +7,27 @@ $('.offers.new, .offers.edit').ready(function() {
     var country_id = e.target.value;
     $.get('/api/provinces/' + country_id);
   }, false);
+
+  document.getElementById('preview_offer_btn').
+    addEventListener('click', function(e) {
+      e.preventDefault();
+      var text = document.getElementById('offer_description').value;
+      var container = document.getElementById('offer__preview');
+      container.innerHTML = textile(text);
+    }, false);
+  //  changes mouse cursor when highlighting loawer right of box
+  $(document).on('mousemove', 'textarea', function(e) {
+    var a = $(this).offset().top + $(this).outerHeight() - 16,	//	top border of bottom-right-corner-box area
+      b = $(this).offset().left + $(this).outerWidth() - 16;	//	left border of bottom-right-corner-box area
+    $(this).css({
+      cursor: e.pageY > a && e.pageX > b ? 'nw-resize' : ''
+    });
+  })
+  //  the following simple make the textbox "Auto-Expand" as it is typed in
+    .on('keyup', 'textarea', function(e) {
+      //  the following will help the text expand as typing takes place
+      while($(this).outerHeight() < this.scrollHeight + parseFloat($(this).css("borderTopWidth")) + parseFloat($(this).css("borderBottomWidth"))) {
+        $(this).height($(this).height()+1);
+      };
+    });
 });
