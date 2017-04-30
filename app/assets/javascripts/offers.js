48793c31dae7738db55427b13f7970c9a44c950f
Karol Moroz <dmuhafc@gmail.com>
2017-04-30 21:14:17 +0800

    Show preview while editing in offers#edit

--- app/assets/javascripts/offers.js
+++ app/assets/javascripts/offers.js
@@ -8,15 +8,8 @@ $('.offers.new, .offers.edit').ready(function() {
     $.get('/api/provinces/' + country_id);
   }, false);
 
-  document.getElementById('preview_offer_btn').
-    addEventListener('click', function(e) {
-      e.preventDefault();
-      var text = document.getElementById('offer_description').value;
-      var container = document.getElementById('offer__preview');
-      container.innerHTML = textile(text);
-    }, false);
   //  changes mouse cursor when highlighting loawer right of box
-  $(document).on('mousemove', 'textarea', function(e) {
+  $('#offer_description').on('mousemove', function(e) {
     var a = $(this).offset().top + $(this).outerHeight() - 16,	//	top border of bottom-right-corner-box area
       b = $(this).offset().left + $(this).outerWidth() - 16;	//	left border of bottom-right-corner-box area
     $(this).css({
@@ -24,7 +17,10 @@ $('.offers.new, .offers.edit').ready(function() {
     });
   })
   //  the following simple make the textbox "Auto-Expand" as it is typed in
-    .on('keyup', 'textarea', function(e) {
+    .on('change keyup paste', function(e) {
+      var text = e.target.value;
+      var container = document.getElementById('offer__preview');
+      container.innerHTML = textile(text);
       //  the following will help the text expand as typing takes place
       while($(this).outerHeight() < this.scrollHeight + parseFloat($(this).css("borderTopWidth")) + parseFloat($(this).css("borderBottomWidth"))) {
         $(this).height($(this).height()+1);
