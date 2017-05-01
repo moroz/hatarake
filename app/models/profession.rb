5fd5bea8f6a05d2346460ecddaa31d72595c8ed1
Karol Moroz <dmuhafc@gmail.com>
2017-05-01 14:45:39 +0800

    Translate candidates#show

--- app/models/profession.rb
+++ app/models/profession.rb
@@ -1,6 +1,7 @@
 class Profession < ApplicationRecord
   include FindOrCreate
   include ActsLikeAutocompletable
+  include Localizable
 
   has_many :candidates
 end
--- spec/models/profession_spec.rb
+++ spec/models/profession_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 require './spec/support/autocompletable'
+require './spec/support/localizable'
 
 RSpec.describe Profession, type: :model do
   include_examples "acts like autocompletable"
+  include_examples "acts like localizable"
 end
