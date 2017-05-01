6f2be237f74c9d68248afa58c05983432c4ef9b7
Karol Moroz <dmuhafc@gmail.com>
2017-05-01 22:48:03 +0800

    Add a JS safe_textilize() function

--- app/assets/javascripts/offers.js
+++ app/assets/javascripts/offers.js
@@ -2,6 +2,11 @@
 // All this logic will automatically be available in application.js.
 // You can use CoffeeScript in this file: http://coffeescript.org/
 
+function safe_textilize(text) {
+  result = text.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;');
+  return textile(result);
+}
+
 $('.offers.new, .offers.edit').ready(function() {
   document.getElementById('offer_country_id').addEventListener('change', function(e) {
     var country_id = e.target.value;
@@ -9,18 +14,19 @@ $('.offers.new, .offers.edit').ready(function() {
   }, false);
 
   //  changes mouse cursor when highlighting loawer right of box
-  $('#offer_description').on('mousemove', function(e) {
-    var a = $(this).offset().top + $(this).outerHeight() - 16,	//	top border of bottom-right-corner-box area
-      b = $(this).offset().left + $(this).outerWidth() - 16;	//	left border of bottom-right-corner-box area
-    $(this).css({
-      cursor: e.pageY > a && e.pageX > b ? 'nw-resize' : ''
-    });
-  })
+  $('#offer_description')
+    .on('mousemove', function(e) {
+      var a = $(this).offset().top + $(this).outerHeight() - 16,	//	top border of bottom-right-corner-box area
+        b = $(this).offset().left + $(this).outerWidth() - 16;	//	left border of bottom-right-corner-box area
+      $(this).css({
+        cursor: e.pageY > a && e.pageX > b ? 'nw-resize' : ''
+      });
+    })
   //  the following simple make the textbox "Auto-Expand" as it is typed in
     .on('change keyup paste', function(e) {
       var text = e.target.value;
       var container = document.getElementById('offer__preview');
-      container.innerHTML = textile(text);
+      container.innerHTML = safe_textilize(text);
       //  the following will help the text expand as typing takes place
       while($(this).outerHeight() < this.scrollHeight + parseFloat($(this).css("borderTopWidth")) + parseFloat($(this).css("borderBottomWidth"))) {
         $(this).height($(this).height()+1);
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def safe_textilize(text)
+    renderer = RedCloth.new(text)
+    renderer.sanitize_html = true
+    renderer.to_html
+  end
 end
