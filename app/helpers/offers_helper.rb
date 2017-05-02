3615c5e289f20b5306e9bf0503aff669ceacbbe9
Karol Moroz <dmuhafc@gmail.com>
2017-05-02 16:04:33 +0800

    Add blank value to province select

--- app/helpers/offers_helper.rb
+++ app/helpers/offers_helper.rb
@@ -1,5 +1,10 @@
 module OffersHelper
-  def localized_collection(collection, value = nil)
-    options_from_collection_for_select(collection, :id, local_name, value)
+  def localized_collection(collection, value = nil, blank: nil)
+    str = ""
+    if blank.present?
+      str = content_tag :option, blank, value: ""
+    end
+    str << options_from_collection_for_select(collection, :id, local_name, value)
+    raw str
   end
 end
