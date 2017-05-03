d6404ea8dd5b2cd0a7c045f697ca5835bdd34cea
Karol Moroz <dmuhafc@gmail.com>
2017-05-03 14:20:28 +0800

    Localize education items

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,5 +1,6 @@
 class Organization < ApplicationRecord
   include FindOrCreate
+  include Localizable
   validates :name_en, presence: true, uniqueness: { allow_nil: true }
   validates :name_pl, uniqueness: { allow_nil: true }
   has_many :work_items
