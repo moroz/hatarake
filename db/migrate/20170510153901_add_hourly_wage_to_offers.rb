b635d184080c3c2e3d1b803beb726a85502d4904
Karol Moroz <dmuhafc@gmail.com>
2017-05-11 00:04:46 +0800

    Add hourly wage range column to offers

---
+++ db/migrate/20170510153901_add_hourly_wage_to_offers.rb
@@ -0,0 +1,5 @@
+class AddHourlyWageToOffers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :offers, :hourly_wage, :numrange
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170508170817) do
+ActiveRecord::Schema.define(version: 20170510153901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,6 +96,7 @@ ActiveRecord::Schema.define(version: 20170508170817) do
     t.datetime "published_at"
     t.string   "slug"
     t.integer  "province_id"
+    t.numrange "hourly_wage"
     t.index ["company_id"], name: "index_offers_on_company_id", using: :btree
     t.index ["country_id"], name: "index_offers_on_country_id", using: :btree
     t.index ["province_id"], name: "index_offers_on_province_id", using: :btree
