d6af9cb9bd4286bdf6911b010f4588257167b245
Karol Moroz <dmuhafc@gmail.com>
2017-05-11 16:20:13 +0800

    Add specs for OffersHelper
    Removes specs for Offer#salary, which is no longer working.
    Adds specs for OffersHelper#readable_currency_range, which does the same
    thing, but is meant to work with ranges.

---
+++ spec/helpers/offers_helper_spec.rb
@@ -0,0 +1,71 @@
+require 'rails_helper'
+
+RSpec.describe OffersHelper do
+  describe "#pretty_currency_value" do
+    context "when value is nil"
+    context "when value is infinity"
+    context "when value is a real number"
+  end
+
+  describe "#localized_currency_value" do
+    context "when value is nil"
+    context "with locale == :pl" do
+      it "uses comma as decimal separator"
+    end
+    context "with locale == :en" do
+      it "uses point as decimal separator"
+    end
+  end
+
+  describe "#readable_currency_range" do
+    def salary_string(format, min, max, currency)
+      I18n.t('currency_range.' + format, min: min, max: max, currency: currency.upcase)
+    end
+
+    context "when min and max present" do
+
+      context "when min == max" do
+        it "returns one value" do
+          # 1000 PLN
+          expect(helper.readable_currency_range((1000..1000), "pln")).to eq(salary_string('mineqmax', 1000, 1000, 'pln'))
+        end
+      end
+
+      context "when min != max" do
+        it "returns a range" do
+          # 1000-2000 PLN
+          expect(helper.readable_currency_range((1000..2000), "pln")).to eq(salary_string('minmax', 1000, 2000, 'pln'))
+        end
+      end
+    end
+
+    context "when only min present" do
+      it "returns min+" do
+        # 1000+ PLN
+        expect(helper.readable_currency_range((1000..Float::INFINITY), "pln")).to eq(salary_string('min', 1000, nil, 'pln'))
+      end
+    end
+
+    context "when only max present" do
+      it "returns up to max" do
+        # up to 2000 PLN
+        expect(helper.readable_currency_range((-Float::INFINITY..2000), "pln")).to eq(salary_string('max', nil, 2000, 'pln'))
+      end
+    end
+
+    context "with -Infinity...Infinity" do
+      it "returns a placeholder string" do
+        # none given
+        expect(helper.readable_currency_range((-Float::INFINITY..Float::INFINITY), "pln")).to eq(salary_string('none', nil, nil, 'pln'))
+      end
+    end
+
+    context "with nil" do
+      it "returns placeholder string" do
+        # none given
+        expect(helper.readable_currency_range(nil, "pln")).to eq(salary_string('none', nil, nil, 'pln'))
+      end
+    end
+
+  end
+end
--- spec/models/offer_spec.rb
+++ spec/models/offer_spec.rb
@@ -42,53 +42,6 @@ RSpec.describe Offer, type: :model do
     end
   end
 
-  describe "salary" do
-    def salary_string(offer, format)
-      I18n.t('offers.salary.' + format, min: offer.salary_min,
-             max: offer.salary_max, currency: offer.currency.upcase)
-    end
-    context "when min and max present" do
-      context "when min == max" do
-        it "returns one value" do
-          offer.salary_min = 1000
-          offer.salary_max = 1000
-          expect(offer.salary).to eq salary_string(offer, 'mineqmax')
-        end
-      end
-
-      context "when min != max" do
-        it "returns a range" do
-          offer.salary_min = 1000
-          offer.salary_max = 2000
-          expect(offer.salary).to eq salary_string(offer, 'minmax')
-        end
-      end
-    end
-
-    context "when only min present" do
-      it "returns min+" do
-        offer.salary_min = 1000
-        offer.salary_max = nil
-        expect(offer.salary).to eq salary_string(offer, 'min')
-      end
-    end
-
-    context "when only max present" do
-      it "returns up to max" do
-        offer.salary_min = nil
-        offer.salary_max = 1000
-        expect(offer.salary).to eq salary_string(offer, 'max')
-      end
-    end
-
-    context "when none present" do
-      it "returns a placeholder string" do
-        offer.salary_min, offer.salary_max = nil
-        expect(offer.salary).to eq salary_string(offer, 'none')
-      end
-    end
-  end
-
   describe "publishing" do
     context "when created" do
       let(:offer) { FactoryGirl.create(:offer) }
