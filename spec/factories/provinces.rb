56b808b9d82e059718fda6baad0c2c024e69bc2c
Karol Moroz <dmuhafc@gmail.com>
2017-05-11 18:08:39 +0800

    Change Polish translations, move offer actions

--- spec/factories/provinces.rb
+++ spec/factories/provinces.rb
@@ -1,7 +1,19 @@
 FactoryGirl.define do
   factory :province do
-    country nil
-    name_en "MyString"
-    name_pl "MyString"
+    country { Country.first || create(:country) }
+    name_en "Brandenburg"
+    name_pl "Brandenburgia"
+
+    trait :mazovia do
+      country { Country.find_by(iso: "PL") || create(:country) }
+      name_en "Mazovia"
+      name_pl "mazowieckie"
+    end
+
+    trait :gp do
+      country { Country.find_by(iso: "PL") || create(:country) }
+      name_en "Greater Poland"
+      name_pl "wielkopolskie"
+    end
   end
 end
