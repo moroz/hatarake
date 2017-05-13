f08f741ad0de606bc2d8f5c21f505c038a22592f
Karol Moroz <dmuhafc@gmail.com>
2017-05-13 12:17:41 +0800

    Change locale links to flags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,12 @@ module ApplicationHelper
     renderer.sanitize_html = true
     renderer.to_html
   end
+
+  def locale_flag(lang)
+    image_tag "/flag-#{lang}.svg", class: 'locale_flag'
+  end
+
+  def locale_link(lang)
+    link_to locale_flag(lang), lang: lang
+  end
 end
