b1036fbec66b1ca1087879e82c49c66ace48a130
Karol Moroz <dmuhafc@gmail.com>
2017-05-16 12:09:42 +0800

    Add website to company profile

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,6 +2,7 @@ class Company < User
   devise :registerable
   validates_presence_of :name
   has_many :offers, dependent: :destroy
+  validates :website, format: { with: URI::regexp }
 
   scope :featured, -> { order('updated_at DESC') }
 
