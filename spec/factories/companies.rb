2d3111c71bc75ca91bd4ae7332bbb3f084903d1a
Karol Moroz <dmuhafc@gmail.com>
2017-05-17 15:00:53 +0800

    Fill in Company unit specs

--- spec/factories/companies.rb
+++ spec/factories/companies.rb
@@ -3,5 +3,6 @@ FactoryGirl.define do
     name "InJobs Inc."
     email { Faker::Internet.email }
     password "foobar2000"
+    website "http://www.example.com"
   end
 end
--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,18 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Company do
+  let(:company) { FactoryGirl.build(:company) }
   describe "validations" do
     describe "website URL format" do
       context "with invalid URL" do
-        it "is invalid"
+        it "is invalid" do
+          company.website = "dupa example ###"
+          expect(company).not_to be_valid
+        end
       end
 
       context "without website" do
-        it "is valid"
+        it "is valid" do
+          company.website = ""
+          expect(company).to be_valid
+        end
       end
 
       context "with valid URL" do
-        it "is valid"
+        it "is valid" do
+          company.website = "http://www.injobs.pl"
+          expect(company).to be_valid
+        end
       end
     end
   end
