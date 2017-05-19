3f8559f8edb9e7291d0ca73a10bdb0616df48377
Karol Moroz <dmuhafc@gmail.com>
2017-05-19 09:06:27 +0800

    Patch migration moving offer location to locations table
    Sorry! Patching old migration! But it hasn't been run in production yet.
    There is probably a better way to migrate data like this, though.

--- db/migrate/20170518070642_move_offer_location_to_table_locations.rb
+++ db/migrate/20170518070642_move_offer_location_to_table_locations.rb
@@ -2,7 +2,7 @@ class MoveOfferLocationToTableLocations < ActiveRecord::Migration[5.0]
   def up
     execute(%q{INSERT INTO locations (country_id, province_id, city, created_at, updated_at) SELECT country_id, province_id, location, NOW(), NOW() FROM offers})
     add_column :offers, :location_id, :integer
-    execute(%q{UPDATE offers AS o SET location_id = l.id FROM locations AS l WHERE o.country_id = l.country_id AND (o.province_id = l.province_id OR (o.province_id IS NULL AND l.province_id IS NULL)) AND o.location = l.city})
+    execute(%q{UPDATE offers AS o SET location_id = l.id FROM locations AS l WHERE o.country_id = l.country_id AND (o.province_id = l.province_id OR (o.province_id IS NULL AND l.province_id IS NULL)) AND (o.location = l.city OR (o.location IS NULL AND l.city IS NULL))})
     remove_column :offers, :location
     remove_column :offers, :province_id
     remove_column :offers, :country_id
