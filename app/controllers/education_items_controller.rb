9f46a4d328f13a1845051c38d0881410a624d80f
Karol Moroz <dmuhafc@gmail.com>
2017-05-25 16:57:34 +0800

    Handle error messages for education items for JS format

--- app/controllers/education_items_controller.rb
+++ app/controllers/education_items_controller.rb
@@ -1,18 +1,26 @@
 class EducationItemsController < ApplicationController
-  expose :education_item
   expose :education_items, -> {current_user.education_items}
+  expose :education_item
+  expose :new_item, -> { current_candidate.education_items.new }
 
   def new
     render 'index'
   end
 
   def create
-    education_item.candidate = current_user
+    education_item.candidate = current_candidate
     if education_item.save
-      redirect_to education_items_path
+      respond_to do |f|
+        f.html { redirect_to education_items_path }
+        f.js
+      end
     else
-      @education_item = education_item
-      render :index
+      respond_to do |f|
+        f.html do
+          render 'index'
+        end
+        f.js { render_js_errors_for education_item }
+      end
     end
   end
 
@@ -24,7 +32,7 @@ class EducationItemsController < ApplicationController
     end
     redirect_to education_items_path
   end
-
+  
   private
 
   def education_item_params
