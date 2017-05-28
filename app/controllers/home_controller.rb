2cc209fe5f89ac040d7bbe0bd6d4bb5b9fe7123c
Karol Moroz <dmuhafc@gmail.com>
2017-05-29 01:45:19 +0800

    Make site name in topbar point to dashboard for users

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,9 @@ class HomeController < ApplicationController
   layout 'home'
 
   def home
+    if signed_in?
+      redirect_to dashboard_path and return
+    end
 		@offers = Offer.includes(:location).featured.limit(5)
     @companies = Company.featured.limit(5)
   end
--- app/controllers/offer_saves_controller.rb
+++ app/controllers/offer_saves_controller.rb
@@ -1,9 +1,21 @@
 class OfferSavesController < ApplicationController
   def create
-    @offer = Offer.friendly.find(params[:offer_id])
-    if @offer.present? && can?(:create, OfferSave)
-      OfferSave.create(offer: @offer, user: current_user)
-      redirect_to @offer, notice: "You have saved this offer."
+    if offer.present? && can?(:create, OfferSave)
+      OfferSave.create(offer: offer, user: current_user)
+      redirect_to offer, notice: "You have saved this offer."
     end
+  end
+
+  def destroy
+    if signed_in? && offer.user_saved?(current_user)
+      OfferSave.where(offer: offer, user: current_user).first.destroy
+      redirect_to offer, notice: "You have unsaved this offer."
+    end
+  end
+
+  private
+
+  def offer
+    @offer ||= Offer.friendly.find(params[:offer_id])
   end
 end
