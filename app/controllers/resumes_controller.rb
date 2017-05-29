ae96f80ba0c9f48d5c5ab7106aa9f866d6855d72
Karol Moroz <dmuhafc@gmail.com>
2017-05-29 15:03:06 +0800

    Change view for resumes#new

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,4 +1,7 @@
 class ResumesController < ApplicationController
+  helper_method :resume
+  authorize_resource
+
   def new
 
   end
@@ -6,6 +9,14 @@ class ResumesController < ApplicationController
   def create
     if resume.save
       redirect_to profile_path, notice: "Success"
+    else
+      respond_to do |f|
+        f.html do
+          flash.now.alert = "There was an error processing your request."
+          render 'new'
+        end
+        f.js { render_js_errors_for resume }
+      end
     end
   end
 
