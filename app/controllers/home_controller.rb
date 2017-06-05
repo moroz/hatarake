c96b0c126877723a1ea37618a7e94ee036cde6b9
Karol Moroz <dmuhafc@gmail.com>
2017-06-05 20:03:41 +0200

    Add eager loading to prevent N+1 queries in home and offers

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
     if signed_in?
       redirect_to dashboard_path and return
     end
-		@offers = Offer.includes(:location).featured.limit(5)
+		@offers = Offer.includes(:company, location: [:country]).featured.limit(5)
     @companies = Company.featured.limit(5)
   end
 end
