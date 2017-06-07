16b9f6451476c0cbbf2e2e94d0c58d247fa82e0b
Karol Moroz <dmuhafc@gmail.com>
2017-06-07 11:44:09 +0200

    Add labels to candidate profile

--- app/helpers/candidates_helper.rb
+++ app/helpers/candidates_helper.rb
@@ -4,4 +4,13 @@ module CandidatesHelper
     now = Time.now.utc.to_date
     now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
   end
+
+  def looking_for_work_label(lfw)
+    classNames = lfw ? 'label success' : 'label alert'
+    text = I18n.t('candidates.show.looking_for_work.' + (!!lfw).to_s)
+    content_tag :span, class: classNames do
+      concat fa_icon 'briefcase'
+      concat ' ' + text
+    end
+  end
 end
