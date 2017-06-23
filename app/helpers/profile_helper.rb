e2fe7f6023ebec6cd4e2c2bc35adf9616401d35a
Karol Moroz <dmuhafc@gmail.com>
2017-06-23 11:46:14 +0200

    Enclose pencil icon in a circle

--- app/helpers/profile_helper.rb
+++ app/helpers/profile_helper.rb
@@ -5,9 +5,7 @@ module ProfileHelper
       image_path('avatar.png')
     content_tag :div, class: "avatar avatar--#{size} #{className}", style: "background-image: url(#{url})" do
       if editable
-        content_tag :div, class: 'avatar__edit_button' do
-          link_to fa_icon('pencil'), edit_avatar_path, title: I18n.t('candidates.show.change_picture')
-        end
+        link_to fa_icon('pencil'), edit_avatar_path, title: I18n.t('candidates.show.change_picture'), class: 'avatar__edit_button'
       end
     end
   end
