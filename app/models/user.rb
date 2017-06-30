19029801c080d6c8c8cc57494ee77bf21caa2ff9
Karol Moroz <dmuhafc@gmail.com>
2017-06-30 10:06:58 +0200

    Style add offer button, make bold font bold again

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   # :registerable, :recoverable, :rememberable, :trackable, :validatable
   devise :database_authenticatable, :rememberable
   validates :email, presence: true, uniqueness: true
-  validates :contact_email, uniqueness: true
-  validates :phone, uniqueness: true
+  validates :contact_email, uniqueness: true, allow_blank: true
+  validates :phone, uniqueness: true, allow_blank: true
   validates :website, format: { with: URI::regexp, allow_blank: true }
   has_one :avatar, foreign_key: 'owner_id'
   extend FriendlyId
