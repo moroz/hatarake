ae794f3a21b57c8e68d12533358ce3cfd223ae47
Karol Moroz <dmuhafc@gmail.com>
2017-06-30 19:31:48 +0200

    Temporarily remove initializer for offsite_payments

--- config/initializers/offsite_payments.rb
+++
@@ -1,6 +0,0 @@
-require 'offsite_payments'
-require 'offsite_payments/action_view_helper'
-
-ActionView::Base.send(:include, OffsitePayments::ActionViewHelper)
-OffsitePayments.mode = :test # for testing server
-Rails.configuration.offsite_payments = YAML.load_file("#{Rails.root}/config/offsite_payments.yml")
