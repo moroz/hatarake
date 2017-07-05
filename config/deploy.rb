b1ac05f7ba06b535ccc3e31bc029ecd38870ed84
Karol Moroz <dmuhafc@gmail.com>
2017-07-05 23:06:35 +0200

    Prepare to deploy in production

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,9 +7,6 @@ set :repo_url, "https://github.com/moroz/hatarake.git"
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
-# Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/home/deployer/injobs"
-
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
 
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,2 +1,3 @@
 set :stage, :staging
 server "217.182.75.140", user: "deployer", roles: %w{app db web}
+set :deploy_to, "/home/deployer/injobs/staging"
