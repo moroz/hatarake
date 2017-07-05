38fc334ef8f53f8cfdd34176398a2d847263cfd1
Karol Moroz <dmuhafc@gmail.com>
2017-07-05 23:06:51 +0200

    Add padding for small screens to offer desc, change views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
 
   def home
 		@offers = Offer.includes(:company, location: [:country]).featured.limit(5)
-    @companies = Company.featured.limit(5)
+    @companies = Company.featured.with_avg_rating.limit(5)
+    @offer_counts = @companies.offer_counts
   end
 end
