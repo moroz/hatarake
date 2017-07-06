6bcdfbfe976e6f75eff22e8d743df7f307359e74
Karol Moroz <dmuhafc@gmail.com>
2017-07-06 13:28:14 +0200

    Make companies look normal on homepage, allow ul, ol, li

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module InJobs
     I18n.available_locales = [:en, :pl]
     I18n.default_locale = :pl
 
-    config.action_view.sanitized_allowed_tags = %w(code blockquote p span a i b em strong br hr img h1 h2 h3 h4 h5 h6 )
+    config.action_view.sanitized_allowed_tags = %w(code blockquote p span a i b em strong br hr img h1 h2 h3 h4 h5 h6 small ul ol li)
     config.action_view.sanitized_allowed_attributes = %w( href src style )
 
     config.time_zone = 'Europe/Warsaw'
