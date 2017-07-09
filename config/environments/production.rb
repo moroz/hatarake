01e2d1fbd0d15def9277c5cc74bb03da61e9576b
Karol Moroz <dmuhafc@gmail.com>
2017-07-09 13:15:31 +0200

    Fix broken environment settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,8 +64,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp-relay.sendinblue.com',
     port: 587,
-    user_name: ENV.fetch('SENDINBLUE_USERNAME'),
-    password: ENV.fetch('SENDINBLUE_API_KEY'),
+    user_name: Rails.application.secrets.SENDINBLUE_USERNAME,
+    password: Rails.application.secrets.SENDINBLUE_API_KEY,
     authentication: 'login',
     enable_starttls_auto: true
   }
