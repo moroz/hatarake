4ee11987cfebd897836749d3cbd5c6050be8a144
Karol Moroz <dmuhafc@gmail.com>
2017-07-09 11:42:31 +0200

    Make carrierwave initializer use Figaro keys

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,9 +2,9 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),                        # required
-    aws_secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),                        # required
-    region:                ENV.fetch('AWS_REGION'),                  # optional, defaults to 'us-east-1'
+    aws_access_key_id:     Figaro.env.AWS_ACCESS_KEY_ID,                        # required
+    aws_secret_access_key: Figaro.env.AWS_SECRET_ACCESS_KEY,                        # required
+    region:                Figaro.env.AWS_REGION,                  # optional, defaults to 'us-east-1'
   }
 
   # For testing, upload files to local `tmp` folder.
@@ -18,7 +18,7 @@ CarrierWave.configure do |config|
 
   config.cache_dir = "#{Rails.root}/tmp/uploads"                  # To let CarrierWave work on heroku
 
-  config.fog_directory  = ENV.fetch('S3_BUCKET_NAME') # required
+  config.fog_directory  = Figaro.env.S3_BUCKET_NAME # required
 
   # config.fog_attributes = { cache_control: "public, max-age=#{365.day.to_i}" } # optional, defaults to {}
 end
