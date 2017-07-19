8f8bcef01dd0a4459ce9980d96e3aa7d1b3d0071
Karol Moroz <dmuhafc@gmail.com>
2017-07-19 11:00:48 +0200

    Disable CSRF verification in PaymentsController

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,4 +1,6 @@
 class PaymentsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def create
     conn = ActiveRecord::Base.connection
     query = ActiveRecord::Base.send(:sanitize_sql_array, ["INSERT INTO raw_payments_data (data) VALUES (?)", request.raw_post])
