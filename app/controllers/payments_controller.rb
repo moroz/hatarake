a9ac9e01fb0b75b450afc83f04c77b7183777df0
Karol Moroz <dmuhafc@gmail.com>
2017-07-19 10:49:50 +0200

    Fix payments#create

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,7 +1,8 @@
 class PaymentsController < ApplicationController
   def create
     conn = ActiveRecord::Base.connection
-    query = ActiveRecord::Base.send(:sanitize_sql_array, ["INSERT INTO raw_payments_data (raw_data) VALUES (?)", request.raw_post])
+    query = ActiveRecord::Base.send(:sanitize_sql_array, ["INSERT INTO raw_payments_data (data) VALUES (?)", request.raw_post])
     conn.execute(query)
+    render plain: 'OK'
   end
 end
