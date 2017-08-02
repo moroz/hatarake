fe10330fe925ef02f4a8c11be23b83bb69b11ddd
Karol Moroz <dmuhafc@gmail.com>
2017-08-02 17:13:19 +0200

    Fix broken controllers

--- app/controllers/dotpay_controller.rb
+++ app/controllers/dotpay_controller.rb
@@ -1,9 +1,10 @@
-class DotpayController < ordersController
+class DotpayController < PaymentsController
   def confirm
     @dotpay = DotpayPayment.new(request.raw_post)
     if @dotpay.acknowledge
-      @order = Order.find_by(description: @dotpay.description)
+      @order = Order.find_by(payment_description: @dotpay.description)
       @order.update(status: @dotpay.status)
+      logger.info "Received Dotpay payment: #{@dotpay.amount} #{@dotpay.currency.upcase}"
       @order.paid!
     else
       logger.error "Incorrect dotpay payment"
