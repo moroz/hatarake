c1649f8e8b773c02cd10e9680418fa1d91b19a77
Karol Moroz <dmuhafc@gmail.com>
2017-08-09 17:01:02 +0200

    Add cookies consent information

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require foundation.dropdownMenu.js
 //= require offers
 //= require companies
+//= require cookies_eu
 //= require candidates
 //= require Markdown.Converter
 //= require Markdown.Sanitizer
