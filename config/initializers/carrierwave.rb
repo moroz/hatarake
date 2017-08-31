f909f1cf995d9a9c7ad02f608c8c9c709bf45536
Karol Moroz <k.j.moroz@gmail.com>
2017-08-31 16:36:19 +0200

    Fix syntax errors in carrierwave initializer

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,9 +2,9 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     ENV.fetch['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV.fetch['AWS_SECRET_ACCESS_KEY'],
-    region:                ENV.fetch['AWS_REGION']
+    aws_access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    region:                ENV.fetch('AWS_REGION')
   }
 
   # For testing, upload files to local `tmp` folder.
@@ -18,7 +18,7 @@ CarrierWave.configure do |config|
 
   config.cache_dir = "#{Rails.root}/tmp/uploads"                  # To let CarrierWave work on heroku
 
-  config.fog_directory  = Rails.application.secrets.s3_bucket # required
+  config.fog_directory  = ENV.fetch('S3_BUCKET_NAME')
 
   # config.fog_attributes = { cache_control: "public, max-age=#{365.day.to_i}" } # optional, defaults to {}
 end
