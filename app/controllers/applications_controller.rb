ad69651d720dff87117abe45c1455926001d2479
Karol Moroz <k.j.moroz@gmail.com>
2017-09-13 22:26:51 +0200

    Add redirect in applications#new to ensure data integrity

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -7,8 +7,13 @@ class ApplicationsController < ApplicationController
   authorize_resource
 
   def new
-    if candidate_signed_in? && offer.candidate_applied?(current_candidate)
-      redirect_to offer, alert: translate_with_gender('applications.already_applied', current_candidate.profile.sex)
+    if candidate_signed_in? 
+      if offer.candidate_applied?(current_candidate)
+        redirect_to offer, alert: translate_with_gender('applications.already_applied', current_candidate.profile.sex) and return
+      end
+      if current_candidate.profile.blank?
+        redirect_to edit_candidate_profile_path, alert: I18n.t('applications.new.candidate_profile_blank') and return
+      end
     end
   end
 
