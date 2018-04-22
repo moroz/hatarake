773d656fc0330fca36a59f0d159e6cabed32c65d
Jan Matuszewski <luirun@gazeta.pl>
2018-04-22 11:29:10 +0200

    Fix company meta tags

--- app/helpers/companies_helper.rb
+++ app/helpers/companies_helper.rb
@@ -1,13 +1,7 @@
 module CompaniesHelper
   def prepare_company_meta_description(company)
-    contact_website = company.website if company.website.present?
     description = ActionView::Base.full_sanitizer.sanitize(company.description) if company.description.present?
-    if description.nil? && contact_website.nil?
-      description = "Niestety firma #{company.name} nie udostępniła nam żadnych informacji o swojej działalności."
-    end
-    if description.nil? && !contact_website.nil?
-      description = "Firma #{company.name} nie uzupełnia jeszcze swojego opisu, jednak możesz znaleźć więcej informacji na stronie #{company.website}."
-    end
+    description = t('companies.show.meta_description', company: company.name) if description.nil?
     return description
   end
 end
